1f8f95ed5bea31ed44363defbb9f63355010e44d
whatsupcoders <whatsupcoders@gmail.com>
2019-10-10 17:38:26 -0500

    updated test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,6 @@ void main() {
   testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
-    expect(find.text('Hello World!'), findsOneWidget);
+    expect(find.text('Hello World'), findsOneWidget);
   });
 }
